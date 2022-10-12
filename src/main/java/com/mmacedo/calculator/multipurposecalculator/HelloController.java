fa204ab7568174f48b43af2aba6a99a9f4216ac5
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-12 19:12:51 -0300

    redesigned application

--- src/main/java/com/mmacedo/calculator/multipurposecalculator/HelloController.java
+++ src/main/java/com/mmacedo/calculator/multipurposecalculator/HelloController.java
@@ -1,14 +1,30 @@
 package com.mmacedo.calculator.multipurposecalculator;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class HelloController {
+    @FXML
+    private BorderPane borderPane;
+
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button imcButton;
+
+    @FXML
+    protected void onHelloButtonClick() throws IOException {
+        AnchorPane view = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("BMICalculator.fxml")));
+        borderPane.setCenter(view);
     }
 }
