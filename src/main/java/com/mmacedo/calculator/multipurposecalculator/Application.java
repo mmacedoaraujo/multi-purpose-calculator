ae013ce01400e40765748584b5632951fee2cead
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-13 10:31:42 -0300

    added interface numbers

--- src/main/java/com/mmacedo/calculator/multipurposecalculator/Application.java
+++ src/main/java/com/mmacedo/calculator/multipurposecalculator/Application.java
@@ -1,20 +1,16 @@
 package com.mmacedo.calculator.multipurposecalculator;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("StandardCalculator.fxml"));
         Scene scene = new Scene(root);
         stage.setTitle("Calculator app");
         stage.setScene(scene);
--- src/main/java/com/mmacedo/calculator/multipurposecalculator/SceneController.java
+++ src/main/java/com/mmacedo/calculator/multipurposecalculator/SceneController.java
@@ -4,27 +4,35 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class HelloController {
+public class SceneController {
     @FXML
     private BorderPane borderPane;
-
+    @FXML
+    private AnchorPane rootPane;
     @FXML
     private Label welcomeText;
 
     @FXML
-    private Button imcButton;
+    private Button standardCalculatorButton;
 
     @FXML
-    protected void onHelloButtonClick() throws IOException {
+    private Button bmiCalculatorButton;
+
+    @FXML
+    protected void onStandardCalculatorButtonButtonClick() throws IOException {
+        borderPane.setCenter(rootPane);
+    }
+
+    @FXML
+    protected void onBmiCalculatorButtonButtonClick() throws IOException {
         AnchorPane view = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("BMICalculator.fxml")));
         borderPane.setCenter(view);
     }
+
 }
