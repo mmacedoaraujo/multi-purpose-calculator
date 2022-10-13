23fed641fd2247ca855119565a5561fcaf05491e
mmacedoaraujo <marcomacedoaraujo@gmail.com>
2022-10-13 12:33:52 -0300

    created clear method

--- src/main/java/com/mmacedo/calculator/multipurposecalculator/MainWindowController.java
+++ src/main/java/com/mmacedo/calculator/multipurposecalculator/MainWindowController.java
@@ -1,10 +1,12 @@
 package com.mmacedo.calculator.multipurposecalculator;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -18,8 +20,7 @@ public class MainWindowController {
     @FXML
     private AnchorPane rootPane;
     @FXML
-    private Label welcomeText;
-
+    private Label lblResult;
     @FXML
     private Button standardCalculatorButton;
 
@@ -27,10 +28,17 @@ public class MainWindowController {
     private Button bmiCalculatorButton;
 
     @FXML
+    private Button btnClear;
+
+    @FXML
     private ImageView btnMinimize;
 
     @FXML
     private ImageView btnClose;
+
+    private String firstNumber = "";
+
+    private String currentNumber = "";
     private double x, y;
 
     public void init(Stage stage) {
@@ -56,6 +64,36 @@ public class MainWindowController {
     protected void onBmiCalculatorButtonButtonClick() throws IOException {
         AnchorPane view = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("BMICalculator.fxml")));
         borderPane.setCenter(view);
+    }
+
+    @FXML
+    void onBtnClearClicked(ActionEvent event) {
+        currentNumber = "";
+        lblResult.setText("");
+    }
+
+    @FXML
+    void onNumberClicked(MouseEvent event) {
+        firstNumber = ((Button) event.getSource()).getText();
+        addNumber(firstNumber);
+    }
+
+
+    void onSymbolClicked(MouseEvent event) {
+        addSymbol(((Button) event.getSource()).getText());
+    }
+
+    public void updateLabelResult() {
+        lblResult.setText(currentNumber);
+    }
+
+    public void addNumber(String number) {
+        currentNumber += number;
+        updateLabelResult();
+    }
+
+    private void addSymbol(String text) {
+
     }
 
 }
